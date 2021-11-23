dae1b894f840dee23401bc78c34a84c6f9dde997
gobalkrishnan-engineer <gobalkrishnan.developer@gmail.com>
2021-11-23 09:41:17 +0530

    as

---
+++ mathematics/mathematics.py
@@ -0,0 +1,11 @@
+mathematics_chapter_vol1=[
+    "Sets, Relations and Function",
+    "Basic Algebra",
+    "Trigonometry",
+    "Combinatorics and Mathematical Induction",
+    "Binomial Theorem, Sequence and Series",
+    "Two Dimensional Analytical Geometry",
+]
+mathematics_chapter_vol2=[
+    
+]
